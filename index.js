a6accb8893bcb42d8fc9b8620353086ace6eddf7
yamuna <yamunajayan@gmail.com>
2024-10-22 16:32:06 -0400

    Revert "Revert "Feature database setup-initial setup""
    This reverts commit 00d398f60c1258d3d5f12cec7b3445f5ad2df73c.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import router from "./routes/router.js";
+import cors from "cors";
 
 const PORT = 8080;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 app.use("/countries", router);
 //test
 
